2c2311e4c56e76275deb72c292fa7eb1b7a02431
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-12-08 18:02:39 +0100

    added sidebar at dashboard

--- src/index.js
+++ src/index.js
@@ -2,4 +2,26 @@ import './main.css'
 import '@fortawesome/fontawesome-free/css/all.min.css'
 import '@fortawesome/fontawesome-free/js/all.min.js'
 
-console.log('Webpack po funksionon!')
+// Modal button
+
+window.openModal = function () {
+  document.getElementById('createModal').classList.remove('hidden')
+  document.getElementById('modalOverlay').classList.remove('hidden')
+}
+
+window.closeModal = function () {
+  document.getElementById('createModal').classList.add('hidden')
+  document.getElementById('modalOverlay').classList.add('hidden')
+}
+
+const circles = document.querySelectorAll('.color-circle')
+
+circles.forEach((circle) => {
+  circle.addEventListener('click', () => {
+    circles.forEach((c) => c.classList.remove())
+
+    circle.classList.add('selected')
+  })
+})
+
+// End Modal button
