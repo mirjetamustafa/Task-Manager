a115f24252f185f6eaffcec192356837c43ff8b9
Mirjeta Mustafa <mustafamirjeta11@gmail.com>
2025-12-11 10:54:10 +0100

    add dashboard functionality

--- src/components/task.js
+++ src/components/task.js
@@ -1,8 +1,19 @@
 export class Task {
-  constructor(id, title, description, completed = false) {
+  constructor(
+    id,
+    title,
+    description,
+    project,
+    status,
+    priority,
+    completed = false
+  ) {
     this.id = id
     this.title = title
     this.description = description
+    this.project = project
+    this.status = status
+    this.priority = priority
     this.completed = completed
   }
 }
--- src/components/taskManager.js
+++ src/components/taskManager.js
@@ -2,12 +2,67 @@ import { Task } from './task.js'
 
 export class TaskManager {
   constructor() {
-    this.tasks = []
+    this.tasks = [
+      {
+        id: 1,
+        title: 'Hello',
+        description:
+          " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        project: 'Work',
+        status: 'todo',
+        priority: 'Medium',
+      },
+      {
+        id: 2,
+        title: 'Hello',
+        description:
+          " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        project: 'Work',
+        status: 'todo',
+        priority: 'Medium',
+      },
+      {
+        id: 3,
+        title: 'Hello',
+        description:
+          " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        project: 'Work',
+        status: 'todo',
+        priority: 'Medium',
+      },
+      {
+        id: 4,
+        title: 'Hello',
+        description:
+          " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        project: 'Work',
+        status: 'todo',
+        priority: 'Medium',
+      },
+      {
+        id: 5,
+        title: 'Hello',
+        description:
+          " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        project: 'Work',
+        status: 'todo',
+        priority: 'Medium',
+      },
+      {
+        id: 6,
+        title: 'sdfs',
+        description:
+          " Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.",
+        project: 'Personal',
+        status: 'in progress',
+        priority: 'hight',
+      },
+    ]
   }
 
-  addTask(title, descrption) {
+  addTask(title, descrption, project, status, priority) {
     const id = Date.now().toString()
-    const task = new Task(id, title, descrption)
+    const task = new Task(id, title, descrption, project, status, priority)
     this.tasks.push(task)
     return task
   }
